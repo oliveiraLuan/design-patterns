e5a492a43dc73bc027e4661a9e38ce4e15560a4d
Luan de Oliveira <luan012012@hotmail.com>
2024-08-02 19:10:15 -0300

    feat: criado classe Server

---
+++ chain-of-responsability/src/Server.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Server {
+    private Map<String, String> users = new HashMap<>();
+    private Middleware middleware;
+
+    public void setMiddleware(Middleware middleware){
+        this.middleware = middleware;
+    }
+
+    public Boolean login(String email, String password){
+        if(middleware.check(email, password)){
+            System.out.println("Autorização bem sucedida");
+            return true;
+        }
+        return false;
+    }
+
+    public void register(String email, String password){
+        users.put(email, password);
+    }
+
+    public Boolean hasEmail(String email){
+        return users.containsKey(email);
+    }
+
+    public Boolean isValidPassword(String email, String password){
+        return users.get(email).equals(password);
+    }
+}
