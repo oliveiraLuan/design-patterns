ba2eef36e3e8a9686deac1f38c00f86111be88d3
Luan de Oliveira <luan012012@hotmail.com>
2024-08-02 19:23:52 -0300

    feat: criado RoleCheckMiddleware

---
+++ chain-of-responsability/src/RoleCheckMiddleware.java
@@ -0,0 +1,17 @@
+/*
+*   Classe concreta - Verifica a role de um usuário
+* */
+public class RoleCheckMiddleware extends Middleware{
+
+    private final String ADMIN_EMAIL = "admin@gmail.com";
+
+    @Override
+    public boolean check(String email, String password) {
+        if(email.equals(ADMIN_EMAIL)){
+            System.out.println("Olá Admin");
+            return true;
+        }
+        System.out.printf("Olá %s%n" + email);
+        return checkNext(email, password);
+    }
+}
