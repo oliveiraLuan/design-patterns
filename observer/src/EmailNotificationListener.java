c915ff822f9194c987219efc2708fb62ae502819
Luan de Oliveira <luan012012@hotmail.com>
2024-08-06 22:57:32 -0300

    feat: implementacoes EmailNotificationListener e LogOpenListener

---
+++ observer/src/EmailNotificationListener.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+import static java.lang.StringTemplate.STR;
+
+
+public class EmailNotificationListener implements EventListener{
+    private String email;
+
+    public EmailNotificationListener(String email){
+        this.email = email;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println(STR."Email para {email}: Alguém fez a operação de {eventType} com o seguinte arquivo: {file.getName()}");
+    }
+}
---
+++ observer/src/LogOpenListener.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class LogOpenListener implements EventListener{
+    private File log;
+
+    public LogOpenListener(String fileName){
+        this.log = new File(fileName);
+    }
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println(STR."Salvar {log} alguém fez a operação de {eventType} com o seguinte arquivo: {file.getName()}");
+    }
+}
