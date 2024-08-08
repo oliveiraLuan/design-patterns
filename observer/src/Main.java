c2ccf207f3b00ea9001c150852ba167be0c34bd4
Luan de Oliveira <luan012012@hotmail.com>
2024-08-07 22:02:28 -0300

    feat: adicionado client

--- observer/src/Main.java
+++ observer/src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public static void main(String[] args) {
+    Editor editor = new Editor();
+    editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+    editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+    try {
+        editor.openFile("test.txt");
+        editor.saveFile();
+    } catch (Exception e) {
+        e.printStackTrace();
     }
 }
