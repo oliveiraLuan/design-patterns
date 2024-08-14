6ff807030f993327733fe3fd5f03668e82fe18f8
Luan de Oliveira <luan012012@hotmail.com>
2024-08-13 21:42:22 -0300

    feat: criado Circle

---
+++ visitor/src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends Dot{
+    private int radius;
+
+    public Circle(int id, int x, int y, int radius) {
+        super(id, x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitCircle(this);
+    }
+}
