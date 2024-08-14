3117d2723234987b8d045e316a31a7f8985181ac
Luan de Oliveira <luan012012@hotmail.com>
2024-08-13 21:50:41 -0300

    feat: criado CompoundShape

---
+++ visitor/src/CompoundShape.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompoundShape implements Shape{
+    public int id;
+    public List<Shape> children = new ArrayList<>();
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitCompoundGraphic(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void add(Shape shape){
+        children.add(shape);
+    }
+}
