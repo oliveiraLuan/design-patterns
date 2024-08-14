a7fa40076ca23fc374e8580e964fbe1767688e4f
Luan de Oliveira <luan012012@hotmail.com>
2024-08-13 21:50:50 -0300

    feat: criado Visitor

---
+++ visitor/src/Visitor.java
@@ -0,0 +1,6 @@
+public interface Visitor {
+    String visitDot(Dot dot);
+    String visitRectangle(Rectangle rectangle);
+    String visitCircle(Circle circle);
+    String visitCompoundGraphic(CompoundShape compoundShape);
+}
