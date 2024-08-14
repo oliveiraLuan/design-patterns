ab9e4623a28624a4c8217403a01f4a197dd28bb4
Luan de Oliveira <luan012012@hotmail.com>
2024-08-13 21:41:52 -0300

    feat: criado Rectangle

---
+++ visitor/src/Rectangle.java
@@ -0,0 +1,51 @@
+public class Rectangle implements Shape{
+    private int id, x, y, z, width, height;
+
+    public Rectangle(int id, int x, int y, int z, int width, int height) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitRectangle(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
