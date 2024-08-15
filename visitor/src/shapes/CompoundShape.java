2c26a78b66ce1c1139c4167be4ff6e88033c6334
Luan de Oliveira <luan012012@hotmail.com>
2024-08-14 22:14:32 -0300

    feat: criado construtor com id

--- visitor/src/shapes/CompoundShape.java
+++ visitor/src/shapes/CompoundShape.java
@@ -7,6 +7,10 @@ public class CompoundShape implements Shape {
     public int id;
     public List<Shape> children = new ArrayList<>();
 
+    public CompoundShape(int id) {
+        this.id = id;
+    }
+
     @Override
     public void move(int x, int y) {
 
