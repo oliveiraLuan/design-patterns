c37136f19077287ffb8812c03c73a5e1a62c8d6c
Luan de Oliveira <luan012012@hotmail.com>
2024-08-17 22:30:45 -0300

    feat: criado DataSourceDecorator

---
+++ decorator/src/DataSourceDecorator.java
@@ -0,0 +1,17 @@
+public class DataSourceDecorator implements DataSource{
+    private DataSource wrapper;
+
+    public DataSourceDecorator(DataSource source){
+        this.wrapper = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrapper.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrapper.readData();
+    }
+}
