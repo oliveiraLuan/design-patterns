7236166c6f93fe69bdefd88dfd193430e2474471
Luan de Oliveira <luan012012@hotmail.com>
2024-08-20 20:40:40 -0300

    feat: criado device Radio

---
+++ bridge/src/implementation/Radio.java
@@ -0,0 +1,42 @@
+package implementation;
+
+public class Radio implements Device{
+    private boolean on;
+    private int volume;
+    private int channel;
+
+    @Override
+    public boolean isEnabled() {
+        return on;
+    }
+
+    @Override
+    public void enable() {
+        on = true;
+    }
+
+    @Override
+    public void disable() {
+        on = false;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public int getChannel() {
+        return channel;
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+}
