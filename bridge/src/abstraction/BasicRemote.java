4e1674c13836273812387bf9b5a4fdbc806a0202
Luan de Oliveira <luan012012@hotmail.com>
2024-08-21 22:45:31 -0300

    feat: criado abstração BasicRemote

---
+++ bridge/src/abstraction/BasicRemote.java
@@ -0,0 +1,40 @@
+package abstraction;
+
+import implementation.Device;
+
+public class BasicRemote implements Remote{
+    protected Device device;
+
+    @Override
+    public void power() {
+        System.out.println("Remote: Acionado botão ligar");
+        if(device.isEnabled()){
+            device.disable();
+        }
+        else device.enable();
+    }
+
+    @Override
+    public void volumeDown() {
+        System.out.println("Remote: Acionado botão diminuir volume");
+        device.setVolume(device.getVolume() - 10);
+    }
+
+    @Override
+    public void volumeUp() {
+        System.out.println("Remote: Acionado botão aumentar volume");
+        device.setVolume(device.getVolume() + 10);
+    }
+
+    @Override
+    public void channelDown() {
+        System.out.println("Remote: Canal anterior");
+        device.setChannel(device.getChannel() - 1);
+    }
+
+    @Override
+    public void channelUp() {
+        System.out.println("Remote: Próximo canal");
+        device.setChannel(device.getChannel() + 1);
+    }
+}
