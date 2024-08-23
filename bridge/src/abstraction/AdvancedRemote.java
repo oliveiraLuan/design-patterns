3050de6e9b5c261f7c2a56e266d4788802c55bf2
Luan de Oliveira <luan012012@hotmail.com>
2024-08-22 21:33:40 -0300

    feat: criado AdvancedRemote

---
+++ bridge/src/abstraction/AdvancedRemote.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+import implementation.Device;
+
+public class AdvancedRemote extends BasicRemote{
+    public AdvancedRemote(Device device){
+        this.device = device;
+    }
+    public void mute(){
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
