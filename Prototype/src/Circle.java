2888771489fb3145fd4750b2ed2364130b491bac
Luan de Oliveira <luan012012@hotmail.com>
2024-08-30 21:39:38 -0300

    feat: criado Circle

---
+++ Prototype/src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle extends Shape{
+    private int radius;
+
+    public Circle(){}
+
+    public Circle(Circle target){
+        super(target);
+        if(target != null)
+            this.radius = target.radius;
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+    @Override
+    public boolean equals(Object object2){
+        if(!(object2 instanceof Circle) || !super.equals(object2))
+            return Boolean.FALSE;
+        Circle shape2 = (Circle) object2;
+        return shape2.radius == radius;
+    }
+}
