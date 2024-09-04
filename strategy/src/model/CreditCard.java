2c4eec0832616add05399707ae4a279491853c99
Luan de Oliveira <luan012012@hotmail.com>
2024-09-03 23:03:50 -0300

    refactor: alterado CreditCard para classe

--- strategy/src/model/CreditCard.java
+++ strategy/src/model/CreditCard.java
@@ -1,3 +1,46 @@
 package model;
 
-public record CreditCard(int amount, String number, String date, String cvv) {}
+public class CreditCard {
+    private int amount;
+    private String number;
+    private String cvv;
+    private String date;
+
+    public CreditCard(String number, String cvv, String date) {
+        this.number = number;
+        this.cvv = cvv;
+        this.date = date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
