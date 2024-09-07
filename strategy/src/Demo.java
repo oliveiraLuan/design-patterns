3311ea29435fe1c0b1ac319e20f2aa6e89055f7a
Luan de Oliveira <luan012012@hotmail.com>
2024-09-07 12:30:04 -0300

    fix: corrigindo cálculo do total da compra

--- strategy/src/Demo.java
+++ strategy/src/Demo.java
@@ -16,6 +16,7 @@ public class Demo {
     private static PayStrategy strategy;
 
     private static String PAY = "P";
+    private static String YES = "Y";
 
     static{
         priceOnProducts.put(1, 2200);
@@ -31,20 +32,21 @@ public class Demo {
             String continueChoice = "";
 
             do{
-                System.out.println("Por favor escolha um dos produtos: \n 1 - Mother board \n 2- CPU\n 3- HDD\n 4-Memory");
+                System.out.println("Por favor escolha um dos produtos: \n 1- Mother board \n 2- CPU\n 3- HDD\n 4- Memory");
                 int choice = Integer.parseInt(READER.readLine());
                 cost = priceOnProducts.get(choice);
-                System.out.println("Quantidade:");
+                System.out.println("Digite a quantidade do produto:");
                 int quantity = Integer.parseInt(READER.readLine());
-                order.setTotalCost(cost * quantity);
-                System.out.println("Você deseja continuar selecionando produtos? S/N");
+                int total = cost * quantity;
+                order.setTotalCost(order.getTotalCost() + total);
+                System.out.println("Você deseja continuar selecionando produtos? Y/N");
                 continueChoice = READER.readLine();
-            } while (continueChoice.equals("Y")); {
+            } while (continueChoice.equals(YES)); {
                 if(strategy == null){
                     System.out.println("Por favor, selecione um método de pagamento: \n 1- Pay Pal\n 2- Credit card");
                     String paymentMethod = READER.readLine();
 
-                    strategy = paymentMethod.equals(StragegyEnum.PAYPAL) ? new PayByPayPal() : new PayByCreditCard();
+                    strategy = paymentMethod.equals(StragegyEnum.PAYPAL.getOption()) ? new PayByPayPal() : new PayByCreditCard();
 
                     order.processOrder(strategy);
 
